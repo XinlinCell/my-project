2788fa42f4584e4ae599f762adf37ce86b329011
XinlinCell <3254292405@qq.com>
2020-05-20 10:48:10 +0800

    作业5 05-dingzexin add post youdao

--- text_post_youdao.py
+++ text_post_youdao.py
@@ -3,21 +3,19 @@ from unittest import mock
 
 from post_youdao import *
 
-
-
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
-
     def test_get_ts(self):
-        #import time
-        #t=time.time()
-        #ts=str(int(round(t*1000)))
-        #print(ts)
-        get_ts=mock.Mock(return_value='1588731443713')
-        self.assertEqual('1588731443713',get_ts())
+        get_ts=mock.Mock(return_value='18468439930')
+        self.assertEqual('18468439930',get_ts())
 
-
+    def test_get_salt(self):
+        get_salt=mock.Mock(return_value='18468439930')
+        self.assertEqual('18468439930',get_salt())
+    def test_get_sign(self):
+        get_sign=mock.Mock(return_value='b2ba12b30ffe0261057d8e59fcbe39cc')
+        self.assertEqual('b2ba12b30ffe0261057d8e59fcbe39cc',get_sign())
 
 if __name__ == '__main__':
     unittest.main()
